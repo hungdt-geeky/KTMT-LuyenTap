e35cd595ae50f1aefd05564b9c458d96a734d240
Dam Tien Hung <damtienhung_t62@hus.edu.vn>
2021-01-04 20:36:17 +0700

    Update document ADC

--- bai6.c
+++ bai6.c
@@ -4,7 +4,7 @@
 #include <stdlib.h>
 #include <stdio.h>
 unsigned int gia_tri_adc;
-#define ADC_VREF_TYPE ((0<<REFS1) | (0<<REFS0) | (0<<ADLAR))
+#define ADC_VREF_TYPE ((0<<REFS1) | (0<<REFS0) | (0<<ADLAR)) // chú ý ADLAR=0 dùng cho 10bit và ADLAR=1 dùng cho 8bit.
 
 char buffer[32];
 void updateLCD(int c) {
@@ -14,16 +14,21 @@ void updateLCD(int c) {
 }
 
 unsigned int read_adc(unsigned char adc_input) {
-    ADMUX=adc_input | ADC_VREF_TYPE;
+    ADMUX=adc_input | ADC_VREF_TYPE;  // Chọn kênh ADC và điện áp so sánh, or bit 
+	//	A	B	A OR B
+	//	0	0	0
+	//	0	1	1
+	//	1	0	1
+	//	1	1	1
     delay_us(10);
-    ADCSRA|=(1<<ADSC);
-    while ((ADCSRA & (1<<ADIF))==0);
-    ADCSRA|=(1<<ADIF);
-    return ADCW;
+    ADCSRA|=(1<<ADSC); // cho phép chuyển đổi ADC, khi chuyển đổi xong nó tự về 0, muốn chuyển đổi tiếp ta lại phải set là 1
+    while ((ADCSRA & (1<<ADIF))==0); // Trong khi cờ ngắt vẫn là 0 thì vẫn chuyển đổi adc
+    ADCSRA|=(1<<ADIF); // Khi quá trình chuyển đổi hoàn tất thì cờ ngắt là 1
+    return ADCW; //Giá trị ADC 10bit nếu muốn 8 bit thì return ADCH;
 }
 void main(void) {
-    ADMUX=ADC_VREF_TYPE;
-    ADCSRA=(1<<ADEN) | (0<<ADSC) | (0<<ADFR) | (0<<ADIF) | (0<<ADIE) | (0<<ADPS2) | (1<<ADPS1) | (1<<ADPS0);
+    ADMUX=ADC_VREF_TYPE; // Đặt áp tham chiếu ban đầu
+    ADCSRA=(1<<ADEN) | (0<<ADSC) | (0<<ADFR) | (0<<ADIF) | (0<<ADIE) | (0<<ADPS2) | (1<<ADPS1) | (1<<ADPS0); // khởi tạo  thanh ghi điểu khiển ADC ban đầu
     SFIOR=(0<<ACME);
     DDRD =0x90;
     PORTD.4 = 0;
